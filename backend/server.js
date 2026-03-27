8824b3a6d81128c98e56584adfe7fef63fc09c2f
mehdibahri12-a12y <mehdi.bahri12@gmail.com>
2026-03-27 20:35:11 +0100

    from compass to cloud mongoDB atlas

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/organic-store')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
